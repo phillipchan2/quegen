98aa2f0e201b668ed8c8181ac920566b9db51f80
phillipchan2 <phillip.chan@liferay.com>
2018-09-11 23:20:27 -0700

    update client delivery from server

--- server/server.js
+++ server/server.js
@@ -8,11 +8,10 @@ app.get('/api/hello', (req, res) => {
 	res.send({ express: 'Hello From Express' });
 });
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	});
-}
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
