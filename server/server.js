021fa4fd4fadbbb9bbe88104c9b70d7e0111f73d
phillipchan2 <phillip.chan@liferay.com>
2018-09-11 23:17:51 -0700

    SF

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 5000;
 app.get('/api/hello', (req, res) => {
 	res.send({ express: 'Hello From Express' });
 });
+
 if (process.env.NODE_ENV === 'production') {
-	// Serve any static files
 	app.use(express.static(path.join(__dirname, 'client/build')));
-	// Handle React routing, return all requests to React app
+
 	app.get('*', function(req, res) {
 		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 	});
