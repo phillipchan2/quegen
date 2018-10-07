856e89ef997c330f6ec2b9690ea6066a5a543943
phillipchan2 <phillip.chan@liferay.com>
2018-10-07 08:21:44 -0700

    Make redirect after save

--- client/src/components/pages/AddEditQuestionnaire/AddEditQuestionnaire.js
+++ client/src/components/pages/AddEditQuestionnaire/AddEditQuestionnaire.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
 // components
 import {
@@ -220,143 +220,167 @@ class AddEditQuestionnaire extends Component {
 				) : (
 					''
 				)}
-				<Menu secondary style={{ marginLeft: 0 }}>
-					<Header as="h2">Questionnaire</Header>
 
-					<Menu.Menu position="right">
-						<Menu.Item>
-							<Link
-								target="_blank"
-								to={`/questionnaires/${
-									this.state.currentQuestionnaire._id
-								}`}
-							>{`/questionnaires/${
-								this.state.currentQuestionnaire._id
-							}`}</Link>
-						</Menu.Item>
-						<Menu.Item name="logout" onClick={this.handleItemClick}>
-							{this.state.currentQuestionnaire.published ? (
-								<Button disabled>
-									<Icon disabled name="checkmark" />
-									Published
-								</Button>
-							) : (
-								<Button primary>Publish</Button>
-							)}
-						</Menu.Item>
-					</Menu.Menu>
-				</Menu>
-				<Form>
-					<Form.Field>
-						<label>Name</label>
-						<input
-							name="name"
-							placeholder="Name"
-							value={this.state.currentQuestionnaire.name}
-							onChange={this.handleChange.bind(this)}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<label>Description</label>
-						<textarea
-							name="description"
-							placeholder="description"
-							value={this.state.currentQuestionnaire.description}
-							onChange={this.handleChange.bind(this)}
-						/>
-					</Form.Field>
-					<Form.Field>
-						<Dropdown
-							fluid
-							selection
-							label="Associated Category Set"
-							options={this.state.categorySets.map(
-								categorySet => {
-									return {
-										key: categorySet._id,
-										text: categorySet.name,
-										value: categorySet._id
-									};
-								}
-							)}
-							defaultValue={this.state.currentCategorySet._id}
-							onChange={this.handleCategorySetChange.bind(this)}
-							placeholder="Associated Category Set"
-						/>
-					</Form.Field>
-				</Form>
-				<Divider />
-				<Header as="h3">Questions</Header>
-				{this.state.currentCategorySet._id ? (
-					<div>
-						<Menu secondary vertical>
-							<Dropdown item text="Add Question">
-								<Dropdown.Menu>
-									<Dropdown.Header>
-										Question Type
-									</Dropdown.Header>
-									<Dropdown.Item
-										data-type="weighted"
-										onClick={this.handleAddQuestionClick.bind(
-											this
-										)}
-									>
-										Weighted
-									</Dropdown.Item>
-									<Dropdown.Item>Text</Dropdown.Item>
-									<Dropdown.Item>
-										Multiple Choice
-									</Dropdown.Item>
-								</Dropdown.Menu>
-							</Dropdown>
-						</Menu>
-						{this.state.currentQuestionnaire.questions.length >
-						0 ? (
-							<Sortable
-								options={{}}
-								tag="div"
-								ref={c => {
-									if (c) {
-										sortable = c.sortable;
-									}
-								}}
-								onChange={(order, sortable, evt) => {
-									console.log(order);
-								}}
-							>
-								{this.state.currentQuestionnaire.questions.map(
-									(question, index) => {
-										switch (question.type) {
-											case 'weighted':
-												return (
-													<EditQuestionWeighted
-														index={index}
-														key={question._id}
-														question={question}
-														categorySet={
-															this.state
-																.currentCategorySet
-														}
-														handleChange={this.handleQuestionChange.bind(
-															this
-														)}
-													/>
-												);
-											default:
-												return 'Question';
-										}
-									}
-								)}
-							</Sortable>
-						) : (
-							'No Questions Yet'
-						)}
-					</div>
+				{this.state.updateSuccess ? (
+					<Redirect to={'/admin/questionnaires'} />
 				) : (
-					'Please select an associated category set first'
-				)}
+					<div>
+						<Menu secondary style={{ marginLeft: 0 }}>
+							<Header as="h2">Questionnaire</Header>
 
-				<Button onClick={this.handleSubmit.bind(this)}>Save</Button>
+							<Menu.Menu position="right">
+								<Menu.Item>
+									<Link
+										target="_blank"
+										to={`/questionnaires/${
+											this.state.currentQuestionnaire._id
+										}`}
+									>{`/questionnaires/${
+										this.state.currentQuestionnaire._id
+									}`}</Link>
+								</Menu.Item>
+								<Menu.Item
+									name="logout"
+									onClick={this.handleItemClick}
+								>
+									{this.state.currentQuestionnaire
+										.published ? (
+										<Button disabled>
+											<Icon disabled name="checkmark" />
+											Published
+										</Button>
+									) : (
+										<Button primary>Publish</Button>
+									)}
+								</Menu.Item>
+							</Menu.Menu>
+						</Menu>
+						<Form>
+							<Form.Field>
+								<label>Name</label>
+								<input
+									name="name"
+									placeholder="Name"
+									value={this.state.currentQuestionnaire.name}
+									onChange={this.handleChange.bind(this)}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<label>Description</label>
+								<textarea
+									name="description"
+									placeholder="description"
+									value={
+										this.state.currentQuestionnaire
+											.description
+									}
+									onChange={this.handleChange.bind(this)}
+								/>
+							</Form.Field>
+							<Form.Field>
+								<Dropdown
+									fluid
+									selection
+									label="Associated Category Set"
+									options={this.state.categorySets.map(
+										categorySet => {
+											return {
+												key: categorySet._id,
+												text: categorySet.name,
+												value: categorySet._id
+											};
+										}
+									)}
+									defaultValue={
+										this.state.currentCategorySet._id
+									}
+									onChange={this.handleCategorySetChange.bind(
+										this
+									)}
+									placeholder="Associated Category Set"
+								/>
+							</Form.Field>
+						</Form>
+						<Divider />
+						<Header as="h3">Questions</Header>
+						{this.state.currentCategorySet._id ? (
+							<div>
+								<Menu secondary vertical>
+									<Dropdown item text="Add Question">
+										<Dropdown.Menu>
+											<Dropdown.Header>
+												Question Type
+											</Dropdown.Header>
+											<Dropdown.Item
+												data-type="weighted"
+												onClick={this.handleAddQuestionClick.bind(
+													this
+												)}
+											>
+												Weighted
+											</Dropdown.Item>
+											<Dropdown.Item>Text</Dropdown.Item>
+											<Dropdown.Item>
+												Multiple Choice
+											</Dropdown.Item>
+										</Dropdown.Menu>
+									</Dropdown>
+								</Menu>
+								{this.state.currentQuestionnaire.questions
+									.length > 0 ? (
+									<Sortable
+										options={{}}
+										tag="div"
+										ref={c => {
+											if (c) {
+												sortable = c.sortable;
+											}
+										}}
+										onChange={(order, sortable, evt) => {
+											console.log(order);
+										}}
+									>
+										{this.state.currentQuestionnaire.questions.map(
+											(question, index) => {
+												switch (question.type) {
+													case 'weighted':
+														return (
+															<EditQuestionWeighted
+																index={index}
+																key={
+																	question._id
+																}
+																question={
+																	question
+																}
+																categorySet={
+																	this.state
+																		.currentCategorySet
+																}
+																handleChange={this.handleQuestionChange.bind(
+																	this
+																)}
+															/>
+														);
+													default:
+														return 'Question';
+												}
+											}
+										)}
+									</Sortable>
+								) : (
+									'No Questions Yet'
+								)}
+							</div>
+						) : (
+							'Please select an associated category set first'
+						)}
+
+						<Button onClick={this.handleSubmit.bind(this)}>
+							Save
+						</Button>
+					</div>
+				)}
 			</div>
 		);
 	}
