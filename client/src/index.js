265d0f13b3a9e159cf1eeed96d6344e24282354a
Phillip Chan <phillip.chan@liferay.com>
2018-10-31 10:40:16 -0700

    integrate logrocket in

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import LogRocket from 'logrocket';
+LogRocket.init('tb3mif/quegen');
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
