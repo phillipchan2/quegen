f326d89500c8f9b2bfeec5da5f8954e2dd34eec6
Phillip Chan <phillip.chan@liferay.com>
2018-12-12 08:52:22 -0800

    Add result information on last page

--- client/src/components/pages/Questionnaire/Questionnaire.js
+++ client/src/components/pages/Questionnaire/Questionnaire.js
@@ -68,6 +68,7 @@ class Questionnaire extends Component {
 
 	handleOpen = () => this.setState({ modalOpen: true });
 
+	// seed data
 	UNSAFE_componentWillMount() {
 		axios.get(`/api/quizzes/${this.props.match.params.id}`).then(res => {
 			const questionnaire = res.data.data;
@@ -78,19 +79,17 @@ class Questionnaire extends Component {
 			questionnaireFlow.find((page, index) => {
 				// populate password
 				if (page.name === 'QuestionnaireLogin') {
-
 					// if there is a password
 					if (questionnaire.password) {
 						questionnaireFlow[index].props.password =
-						questionnaire.password;
+							questionnaire.password;
 					} else {
-						questionnaireFlow[index].success = true
+						questionnaireFlow[index].success = true;
 					}
 				}
 
 				// populate questions
 				else if (page.name === 'QuestionnaireQuestions') {
-
 					questionnaireFlow[index].props.questions =
 						questionnaire.questions;
 				}
@@ -111,8 +110,11 @@ class Questionnaire extends Component {
 		var consolidatedSubmissionData = this.state.consolidatedSubmissionData;
 
 		this.setState({
-			consolidatedSubmissionData: Object.assign(consolidatedSubmissionData, data)
-		})
+			consolidatedSubmissionData: Object.assign(
+				consolidatedSubmissionData,
+				data
+			)
+		});
 	}
 
 	handleSubmitData(data) {
@@ -149,14 +151,16 @@ class Questionnaire extends Component {
 						resultCategory: category
 					});
 
-					let quesitonnaireFlow = this.state.questionnaireFlow;
+					let questionnaireFlow = this.state.questionnaireFlow;
 
-					quesitonnaireFlow.find((page, index) => {
-						if (page.name === 'QuestionnaireQuestions') {
-							quesitonnaireFlow[index].success = true;
+					questionnaireFlow.find((page, index) => {
+						if (page.name === 'QuestionnaireResult') {
+							questionnaireFlow[index].resultCategory = category;
+						} else if (page.name === 'QuestionnaireQuestions') {
+							questionnaireFlow[index].success = true;
 
 							this.setState({
-								quesitonnaireFlow: quesitonnaireFlow
+								questionnaireFlow: questionnaireFlow
 							});
 						}
 					});
@@ -214,6 +218,7 @@ class Questionnaire extends Component {
 							handlePageError={this.handlePageError}
 							{...page.props}
 							questionnaireId={this.props.match.params.id}
+							resultCategory={this.state.resultCategory}
 						/>
 					</div>
 				) : (
--- client/src/components/pages/QuestionnaireResult/QuestionnaireResult.js
+++ client/src/components/pages/QuestionnaireResult/QuestionnaireResult.js
@@ -3,7 +3,19 @@ import PropTypes from 'prop-types';
 
 class QuestionnaireResult extends Component {
 	render() {
-		return <div>quesitonnaire results</div>;
+		return (
+			<div>
+				<h1>You are a {this.props.resultCategory.name}</h1>
+				<p>{this.props.resultCategory.resultDescription}</p>
+				{this.props.resultCategory.imageUrl && (
+					<img
+						style={{ width: 300, height: 'auto' }}
+						src={this.props.resultCategory.imageUrl}
+						alt={this.props.resultCategory.name}
+					/>
+				)}
+			</div>
+		);
 	}
 }
 
